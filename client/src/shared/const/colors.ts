f9428f074d1c73254adbf05491e62131c75ca25e
Kent-cpu <74884403+Kent-cpu@users.noreply.github.com>
2023-10-11 23:20:58 +0700

    feat: added ui button

--- client/src/shared/const/colors.ts
+++ client/src/shared/const/colors.ts
@@ -4,6 +4,8 @@ interface Colors {
   orange: string;
   grayDark: string;
   grayLight: string;
+  blueLight: string;
+  blueDark: string;
 }
 
 const colors: Colors = {
@@ -12,6 +14,8 @@ const colors: Colors = {
   orange: 'rgba(255, 166, 64, 1)',
   grayDark: 'rgba(255, 255, 255, 0.32)',
   grayLight: 'rgba(255, 255, 255, 0.72)',
+  blueLight: 'rgba(40,138,232,1)',
+  blueDark: 'rgba(0, 116, 228, 1)',
 };
 
 export { colors, Colors };
---
+++ client/src/shared/ui/button/button.styles.ts
@@ -0,0 +1,62 @@
+import styled from '@emotion/styled';
+import { ButtonSize, ButtonVariant } from 'shared/ui/button/button.component';
+
+interface StyledButtonProps {
+  size: ButtonSize;
+  variant: ButtonVariant;
+  fullWidth: boolean;
+  disabled: boolean;
+}
+
+export const StyledButton = styled.button<StyledButtonProps>((props) => {
+  const { theme, variant, size, fullWidth, disabled } = props;
+
+  const sizeStyled = {
+    s: '32px',
+    m: '38px',
+    l: '40px',
+    xl: '60px',
+  };
+
+  const variantStyles = {
+    text: {
+      background: 'none',
+    },
+    outline: {
+      background: 'transparent',
+      border: '1px solid rgba(245, 245, 245, 0.6)',
+      '&:hover': {
+        backgroundColor: 'rgba(56, 56, 56, 1)',
+      },
+    },
+    contained: {
+      background: theme.colors.blueDark,
+      '&:hover': {
+        backgroundColor: theme.colors.blueLight,
+      },
+    },
+  };
+
+  return {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: fullWidth && '100%',
+    height: sizeStyled[size],
+    padding: '0 5px',
+    cursor: disabled ? 'default' : 'pointer',
+    opacity: disabled ? 0.5 : 1,
+    pointerEvents: disabled ? 'none' : 'auto',
+    borderRadius: '4px',
+    color: theme.colors.white,
+    fontWeight: '500',
+    textTransform: 'uppercase',
+    wordBreak: 'break-word',
+    transition:
+      'background-color 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms,' +
+      'border-color 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms,' +
+      'opacity 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms,' +
+      'color 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms',
+    ...variantStyles[variant],
+  };
+});
