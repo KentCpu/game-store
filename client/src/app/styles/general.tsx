f3e83bcdad54941e95d1ffaef9109fcd44c7ab33
Kent-cpu <74884403+Kent-cpu@users.noreply.github.com>
2023-10-12 21:18:45 +0700

    feat: added fonts

---
+++ client/src/app/styles/general.tsx
@@ -0,0 +1,16 @@
+import { Global, css } from '@emotion/react';
+
+export const GeneralStyle = () => {
+  return (
+    <Global
+      styles={css`
+        @import url('https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&family=Open+Sans:wght@400;500&display=swap');
+
+        body {
+          font-family: 'Inter', 'Open Sans', sans-serif;
+          font-size: 16px;
+        }
+      `}
+    />
+  );
+};
